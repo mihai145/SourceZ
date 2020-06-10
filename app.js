f76518306bcb0da0ed1b693c5e0902d928a53b6d
unknown <opreamihai01@yahoo.com>
2020-06-10 20:37:11 +0300

    express set up

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+///-----------------------///
+///APP SETUP
+///-----------------------///
+app.set("view engine", "ejs");
+
+///-----------------------///
+///ROOT
+///-----------------------///
+app.get("/", (req, res) => {
+    res.render("root.ejs");
+})
+
+///-----------------------///
+///CATCH ALL
+///-----------------------///
+app.get("*", (req, res) => {
+    res.render("notFound.ejs");
+});
+
+///-----------------------///
+///START SERVER///
+///-----------------------///
+app.listen(3000, () => {
+    console.log("Server started. Listening to port 3000...");
+});
