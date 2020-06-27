8daf8e362233b47ccd0051a4e5e0059183888fdb
unknown <opreamihai01@yahoo.com>
2020-06-27 12:54:53 +0300

    app ready

--- app.js
+++ app.js
@@ -74,6 +74,12 @@ app.use(miscRoutes);
 ///-----------------------///
 ///START SERVER
 ///-----------------------///
-app.listen(3000, () => {
-    console.log("Server started. Listening to port 3000...");
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, () => {
+    //console.log("Server started. Listening to port 3000...");
 });
